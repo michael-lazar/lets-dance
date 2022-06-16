8ee71bfc71856dde4110b1aa376580f2f06416a4
Michael Lazar <lazar.michael22@gmail.com>
2022-06-15 22:34:02 -0400

    Some changes for production-readiness

--- letsdance/settings.py
+++ letsdance/settings.py
@@ -2,17 +2,25 @@ from __future__ import annotations
 
 import os
 
+import environ
+
+env = environ.Env()
+env.smart_cast = False
+
 BASE_DIR = os.path.dirname(__file__)
 
 NAME = "lets-dance"
 VERSION = "0.0.1"
 USER_AGENT = f"{NAME}/{VERSION}"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "PLEASE_REPLACE_ME!")
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = env.bool("DEBUG", True)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+if DEBUG:
+    SECRET_KEY = env.str("SECRET_KEY", "PLEASE_REPLACE_ME!")
+else:
+    SECRET_KEY = env.str("SECRET_KEY")
 
 ALLOWED_HOSTS = ["*"]
 
